c0cf6246c17c31a1d1065aee2f99e4a4f1059474
laynia <vc_mikuu@hotmail.com>
2024-08-03 20:39:29 +0200

    Upgrading to 1.37

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,28 @@
 
 #include "scotland2/shared/modloader.h"
 
+#include "custom-types/shared/register.hpp"
+
+#include "GlobalNamespace/StandardLevelDetailViewController.hpp"
+using namespace GlobalNamespace;
+
+#include "UnityEngine/GameObject.hpp"
+#include "UnityEngine/Transform.hpp"
+#include "UnityEngine/RectTransform.hpp"
+#include "UnityEngine/Vector2.hpp"
+#include "UnityEngine/Vector3.hpp"
+#include "UnityEngine/Color.hpp"
+using namespace UnityEngine;
+
+#include "UnityEngine/UI/Button.hpp"
+using namespace UnityEngine::UI;
+
+#include "HMUI/ImageView.hpp"
+using namespace HMUI;
+
+
+
+
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // Stores the ID and version of our mod, and is sent to
 // the modloader upon startup
@@ -13,6 +35,31 @@ Configuration &getConfig() {
   static Configuration config(modInfo);
   return config;
 }
+
+MAKE_HOOK_MATCH(m_DidActivate,
+                &GlobalNamespace::StandardLevelDetailViewController::DidActivate,
+                void,
+                GlobalNamespace::StandardLevelDetailViewController* self,
+                bool firstActivation,
+                bool addedToHeirarchy,
+                bool screenSystemEnabling) {
+
+    m_DidActivate(self, firstActivation, addedToHeirarchy, screenSystemEnabling);
+
+
+    auto* imageCoverTransform = self->get_transform()->Find("LevelDetail/LevelBarBig/SongArtwork")->GetComponent<RectTransform*>();
+
+    imageCoverTransform->set_sizeDelta(Vector2(70.5, 58.0));
+    imageCoverTransform->set_localPosition(Vector3(-34.4, -56, 0));
+    imageCoverTransform->SetAsFirstSibling();
+
+    auto* imageView = imageCoverTransform->GetComponent<ImageView*>();
+
+    imageView->set_color(Color(0.5, 0.5, 0.5, 1));
+    imageView->set_preserveAspect(false);
+    imageView->skew = 0.0f;
+}
+
 
 // Called at the early stages of game loading
 MOD_EXTERN_FUNC void setup(CModInfo *info) noexcept {
