422b3c2e98a5824053f55c50e3001ccada6d29f3
laynia <vc_mikuu@hotmail.com>
2024-08-04 23:01:01 +0200

    Trying to fix logger #9

--- src/main.cpp
+++ src/main.cpp
@@ -21,17 +21,6 @@ using namespace UnityEngine::UI;
 #include "HMUI/ImageView.hpp"
 using namespace HMUI;
 
-// Stores the ID and version of our mod, and is sent to
-// the modloader upon startup
-
-// Loads the config from disk using our modInfo, then returns it for use
-// other config tools such as config-utils don't use this config, so it can be
-// removed if those are in use
-Configuration &getConfig() {
-  static Configuration config(modInfo);
-  return config;
-}
-
 
 MAKE_HOOK_MATCH(m_DidActivate,
                 &GlobalNamespace::StandardLevelDetailViewController::DidActivate,
