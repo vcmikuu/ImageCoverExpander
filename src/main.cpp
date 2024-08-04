a96e8d1f7315f17147948dcbc74874ebda607e2c
laynia <vc_mikuu@hotmail.com>
2024-08-05 01:10:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ MAKE_HOOK_MATCH(m_DidActivate,
 
     imageView->set_color(Color(0.5, 0.5, 0.5, 1));
     imageView->set_preserveAspect(false);
-    imageView->skew;
+    imageView_skew(0.0);
     imageView->__Refresh();
 }
 
